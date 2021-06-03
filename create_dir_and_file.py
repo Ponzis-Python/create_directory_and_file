a2ac1fc887012c0b62ae7d480145c54a0dc41660
Ponzi <43649035+Ponzis-Python@users.noreply.github.com>
2021-06-03 10:18:55 -0400

    Update create_dir_and_file.py

--- create_dir_and_file.py
+++ create_dir_and_file.py
@@ -1,23 +1,19 @@
+#!/usr/bin/env python
+
 import os, errno
 import sys
 from pathlib import Path
-#Allows creation of subdirectory and file in root directories,
-#without running program with sudo
 
-fuddit = (os.path.isdir("/var/log/updates/ponzi/foo"))
+if os.geteuid() != 0:
+    os.execvp('sudo', ['sudo', 'python3'] + sys.argv)
 
-if fuddit==False:
-    if os.geteuid() != 0:
-        os.execvp('sudo', ['sudo', 'python3'] + sys.argv)
+try:
+    daDir = input("Paste new subdirectory name after ponzi here: ")
+    os.makedirs('/var/log/updates/ponzi/{}'.format(daDir))
+    os.chmod('/var/log/updates/ponzi/{}'.format(daDir), 0o775)
+    Path('/var/log/updates/ponzi/{}'.format(daDir) + '/' + 'foo.log').touch()
+    os.chmod('/var/log/updates/ponzi/{}'.format(daDir) + '/' + 'foo.log+', 0o777)
 
-    try:
-        os.makedirs('/var/log/updates/ponzi/foo')
-        os.chmod('/var/log/updates/ponzi/foo', 0o755)
-        Path('/var/log/updates/ponzi/foo/dafoofoo.txt').touch()
-        os.chmod('/var/log/updates/ponzi/foo/dafoofoo.txt', 0o777)
-
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
-else:
-    print("directory exists")
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        raise
